37ad3b6eab87856eb3a2ccaa0f538a2cb2e12369
AlonsoChavez363 <122044170@upq.edu.mx>
2025-05-19 15:25:45 -0600

    Crear un arrow function que reciba de parámetros: nombre y edad y regrese una cadena de texto.

--- practica1/practica1.js
+++ practica1/practica1.js
@@ -10,3 +10,7 @@ console.log(saludo);
 //pasar una funcion tradicional a una arrow
 const cuadrado = (numero) => numero * numero;
 console.log(cuadrado(2)); 
+
+//Crear una arrow function llamada saludopersonalizado que reciba nombre y edad y retorne una cadena
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+console.log(saludoPersonalizado("Alonso", 21));
