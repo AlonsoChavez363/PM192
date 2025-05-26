ef49fa99af91bf0723355f1d0b19e27fea3869d0
AlonsoChavez363 <122044170@upq.edu.mx>
2025-05-26 11:00:31 -0600

    verificar usuario

---
+++ practica3/usuarios.js
@@ -0,0 +1,10 @@
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
+
