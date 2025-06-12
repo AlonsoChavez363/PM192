9e9e55d44f86f19171c2f10d4bb608bbe9c5d346
AlonsoChavez363 <122044170@upq.edu.mx>
2025-06-11 18:19:20 -0600

    primer avance con texto y boton

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,29 @@
+/* Zona 1: Importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native'; /* Poner todos los componentes que vamos a utilzar */
 
+const Texto=(props)=>{
+  const {contenido}=props
+  return(
+    <text>{contenido}</text>
+  )
+}
+
+/* Zona 2: Main(ejecutacion) */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="Mundo"></Texto>
+      <Texto contenido="React_Native"></Texto>
+
+      <Button title="Presioname!"> </Button>
     </View>
   );
 }
 
+/* Zona 3: Estetica de la pagina */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
